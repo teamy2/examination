8aafe7ce2176fe5f8029b7fc16e69c3a52a70aa8
reubenjds <reubenjds@gmail.com>
2023-10-14 16:26:08 -0400

    feat: added boilerplate pages

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,22 @@
+<div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+    <form class="card-body">
+      <div class="form-control">
+        <label class="label">
+          <span class="label-text">Email</span>
+        </label>
+        <input type="email" placeholder="email" class="input input-bordered" required />
+      </div>
+      <div class="form-control">
+        <label class="label">
+          <span class="label-text">Password</span>
+        </label>
+        <input type="password" placeholder="password" class="input input-bordered" required />
+        <label class="label">
+          <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+        </label>
+      </div>
+      <div class="form-control mt-6">
+        <button class="btn btn-primary">Login</button>
+      </div>
+    </form>
+  </div>
