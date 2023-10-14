3c63a5eafa626a7899bdbab456021eb707986a50
Robert Zuchniak <robert.zuchniak@hotmail.com>
2023-10-14 17:27:54 -0400

    add custom colours to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config}*/
+
+const colors = require('tailwindcss/colors')
 const config = {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			blue1: '#1a59bd',
+			blue2: '#0c264f',
+			turquoise: '#189e97'
+
+	},
 		extend: {},
 	},
 	plugins: [
