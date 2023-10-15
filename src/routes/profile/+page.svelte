9d82e90d2c96f0ab8d761158f843601983eaa79f
Robert Zuchniak <robert.zuchniak@hotmail.com>
2023-10-14 20:37:17 -0400

    feat: create profile page and tailwind colours

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,6 +1,6 @@
 <div class="navbar bg-blue2">
   <div class="flex-1">
-    <a class="btn btn-ghost normal-case text-xl">Examination</a>
+    <a class="btn btn-ghost normal-case text-3xl">Examination</a>
   </div>
   <div class="flex-none gap-2">
     <div class="dropdown dropdown-end">
@@ -25,10 +25,42 @@
 
 
 
-<div>
-<div class="avatar">
-  <div class="w-24 rounded-full">
+<div style="display: flex;">
+<div class="avatar p-10">
+  <div class="w-80 h-30">
     <img src="https://i.stack.imgur.com/l60Hf.png" />
   </div>
 </div>
+<div style="padding: 50px">
+<h1 style="font-size: 50px">
+  Username
+</h1>
+<h1>
+  Description
+</h1>
 </div>
+</div>
+
+<div class="flex gap-10 justify-center">
+<div class="card w-96 bg-base-100 shadow-xl image-full">
+  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+<div class="card w-96 bg-base-100 shadow-xl image-full">
+  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+</div>
+
