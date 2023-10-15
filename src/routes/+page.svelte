9c3d9326c48ddae943dce4b9d6f0a4c0033b7b1e
Vasil <vasiltopalovic@outlook.com>
2023-10-15 15:50:23 -0400

    feat: front page styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,4 @@
 <div class="h-full flex-column">
-	<div class="navbar">
-		<div class="flex-1">
-			<a class="btn btn-ghost normal-case text-xl red">Examination</a>
-		</div>
-	</div>
-
 	<div class="hero h-5/6">
 		<div class="hero-content text-center">
 			<div class="">
@@ -16,10 +10,10 @@
 	</div>
 
 	<svg
+		class="fill-primary w-full max-h-32 absolute bottom-0"
 		xmlns="http://www.w3.org/2000/svg"
 		viewBox="0 0 1200 180"
 		preserveAspectRatio="none"
-		class="fill-primary w-full block relative max-h-32 ab"
 		><path
 			d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V180H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
 		/></svg
