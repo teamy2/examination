5c0791169385912164edeadf770edf21349d023c
Reuben D'Souza <reubenjds@gmail.com>
2023-10-15 15:21:11 -0400

    feat: add QuizAnswers component

---
+++ src/lib/components/QuizAnswers.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { quizzes } from "$lib/quizzes";
+
+	let answers = 4;
+</script>
+
+<div class="card w-full bg-base-200 shadow-xl">
+	<div class="card-body">
+		<input
+			type="text"
+			placeholder="Type Question Here..."
+			class="input w-full max-w-xs card-title"
+		/>
+
+		<div class="grid gap-2">
+			{#each { length: answers } as _, index}
+				<div class="flex flex-row items-center">
+					<input type="checkbox" class="checkbox col-span-1 checkbox-success" />
+					<div>Answer {index + 1}</div>
+				</div>
+			{/each}
+		</div>
+
+		<div class="card-actions justify-end">
+			<button class="btn btn-primary" on:click={() => answers++}
+				>Add Answer</button
+			>
+		</div>
+	</div>
+</div>
