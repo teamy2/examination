dcb210ce3ac4eb379271cbc144ff120a7b72700e
Reuben D'Souza <reubenjds@gmail.com>
2023-10-15 14:40:09 -0400

    feat: add QuizQuestions component

--- src/routes/quizzes/create/+page.svelte
+++ src/routes/quizzes/create/+page.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
+	let questionsAmount = 1;
 
-    let questionsAmount = 1;
+	import QuizQuestions from "$lib/components/QuizQuestions.svelte";
 </script>
 
-
-
-
 <div class="pb-5">
-    Create a Quiz!
-    <input type="text" placeholder="Quiz Name" class="input input-bordered input-info w-full max-w-xs" />
-    <label class="btn btn-info" for="bg-select">Choose Background</label>
-    <input id="bg-select" type="file" class="file-input w-full max-w-xs" hidden/>
+	Create a Quiz!
+	<input
+		type="text"
+		placeholder="Quiz Name"
+		class="input input-bordered input-info w-full max-w-xs"
+	/>
+	<label class="btn btn-info" for="bg-select">Choose Background</label>
+	<input id="bg-select" type="file" class="file-input w-full max-w-xs" hidden />
 </div>
 
 <div class="flex justify-center text-center">
-<div class="grid grid-cols-3 gap-3">
-    {#each {length : questionsAmount} as _, index}
-    <input type="text" placeholder="Question {index+1}" class="input input-bordered input-success w-full max-w-xs" />
-    <input type="text" placeholder="Answer" class="input input-bordered input-error w-full max-w-xs" />
-    <input type="file" class="file-input file-input-bordered file-input-primary w-full max-w-xs" />
-    {/each}
-    <button  class="btn btn-success"on:click={() => questionsAmount++}>Add Question!</button>
-</div>
-
+	<div class="grid grid-cols-3 gap-3">
+		{#each { length: questionsAmount } as _, index}
+			<QuizQuestions />
+		{/each}
+	</div>
+	<button class="btn btn-success" on:click={() => questionsAmount++}
+		>Add Question!</button
+	>
 </div>
