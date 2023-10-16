b3329c71d0af475872f48ffc198a363600f9f652
matteopolak <matteopolak@hotmail.com>
2023-10-15 22:48:52 -0400

    feat: add /quizzes/create

--- src/lib/components/QuizQuestions.svelte
+++ src/lib/components/QuizQuestions.svelte
@@ -6,7 +6,7 @@
 
 	export let question = {
 		name: '',
-		options: [] as string[],
+		options: [''] as string[],
 		answers: [] as number[],
 	};
 </script>
@@ -15,42 +15,46 @@
 	<div class="card-body">
 		<button
 			on:click={() => dispatch('remove')}
-			class="btn btn-md btn-circle btn-ghost absolute right-2 top-2 text-2xl"
+			class="btn btn-md btn-circle btn-ghost absolute right-2 top-2 text-xl"
 			>✕</button
 		>
 		<input
 			type="text"
 			placeholder="Question"
-			class="input w-6/12 card-title my-4"
+			class="input w-6/12 card-title mb-4"
 			bind:value={question.name}
 		/>
 
 		{#each { length: answersAmount } as _, index}
-			<div class="flex flex-row items-center gap-4">
+			<div class="flex flex-row items-center gap-2 pl-2">
 				<input
 					type="checkbox"
-					class="checkbox col-span-1 checkbox-success"
+					class="checkbox col-span-1"
 					checked={question.answers.includes(index)}
-					on:click={() => {
+					on:click={() =>
 						question.answers.includes(index)
 							? question.answers.splice(question.answers.indexOf(index), 1)
-							: question.answers.push(index);
-						console.log(question.answers);
-					}}
+							: question.answers.push(index)}
 				/>
 				<input
 					type="text"
 					class="input bg-base-200 w-6/12"
-					placeholder="Answer {index + 1}"
+					placeholder="Insert option..."
 					bind:value={question.options[index]}
 				/>
 			</div>
 		{/each}
-
-		<div class="card-actions justify-end">
-			<button class="btn btn-primary my-4" on:click={() => answersAmount++}
-				>Add Answer</button
+		<button
+			class="flex flex-row items-center gap-6 cursor-pointer bg-base-300 hover:bg-base-100 transition-all duration-300 h-12 p-2 rounded-lg"
+			on:click={() => {
+				question.options[answersAmount] = '';
+				answersAmount++;
+			}}
+		>
+			<svg class="fill-current h-6 w-6" viewBox="0 0 24 24"
+				><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" /></svg
 			>
-		</div>
+			<p class="text-left">Add another answer...</p>
+		</button>
 	</div>
 </div>
--- src/routes/quizzes/create/+page.svelte
+++ src/routes/quizzes/create/+page.svelte
@@ -39,24 +39,24 @@
 	} satisfies Quiz;
 </script>
 
-<div class="flex flex-col gap-4 p-8 items-center">
-	<div class="flex items-center gap-8">
+<div class="flex flex-col gap-6 p-8 items-center w-full">
+	<div class="grid items-center gap-2 w-6/12">
 		<input
 			type="text"
-			placeholder="Quiz Name"
-			class="input input-bordered w-40 max-w-ws"
+			placeholder="Quiz name..."
+			class="input h-14 bg-base-200 w-full text-2xl font-bold"
 			bind:value={q.title}
 		/>
 
 		<textarea
-			class="textarea textarea-bordered"
-			placeholder="Description"
+			class="textarea bg-base-200 w-full h-32"
+			placeholder="Quiz description..."
 			bind:value={q.description}
 		/>
 	</div>
 
 	{#each q.questions as _, index}
-		<div class="py-4 w-6/12">
+		<div class="w-6/12">
 			<QuizQuestions
 				bind:question={q.questions[index]}
 				on:remove={() => {
@@ -68,16 +68,28 @@
 	{/each}
 
 	<button
-		class="btn"
+		class="w-1/2 bg-base-200 h-32 rounded-2xl hover:bg-base-300 transition-all duration-300 flex flex-row items-center gap-6 justify-center"
 		on:click={() => {
 			q.questions.push(defaultQuestion());
 			q = q;
-		}}>Add question</button
+		}}
 	>
+		<svg class="fill-current h-8 w-8" viewBox="0 0 24 24"
+			><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" /></svg
+		>
+		<span class="text-2xl">Add another question...</span>
+	</button>
 
 	<button
-		class="btn"
+		class="w-1/2 bg-base-200 h-32 rounded-2xl hover:bg-primary transition-all duration-300 flex flex-row items-center gap-6 justify-center"
 		disabled={loading || !q.questions.length}
-		on:click={handleSubmit}>Create Quiz!</button
+		on:click={handleSubmit}
 	>
+		<svg class="fill-current h-8 w-8" viewBox="0 0 24 24">
+			<path
+				d="M9.19 6.35c-2.04 2.29-3.44 5.58-3.57 5.89L2 10.69l4.05-4.05c.47-.47 1.15-.68 1.81-.55l1.33.26zM11.17 17s3.74-1.55 5.89-3.7c5.4-5.4 4.5-9.62 4.21-10.57-.95-.3-5.17-1.19-10.57 4.21C8.55 9.09 7 12.83 7 12.83L11.17 17zm6.48-2.19c-2.29 2.04-5.58 3.44-5.89 3.57L13.31 22l4.05-4.05c.47-.47.68-1.15.55-1.81l-.26-1.33zM9 18c0 .83-.34 1.58-.88 2.12C6.94 21.3 2 22 2 22s.7-4.94 1.88-6.12C4.42 15.34 5.17 15 6 15c1.66 0 3 1.34 3 3zm4-9c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2z"
+			/>
+		</svg>
+		<span class="text-2xl">Create quiz</span>
+	</button>
 </div>
