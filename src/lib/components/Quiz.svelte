5bf7adec7c3e1dafb31f9d57af3c9d93368d684f
matteopolak <matteopolak@hotmail.com>
2023-10-15 22:17:37 -0400

    feat: improve quizzes page

--- src/lib/components/Quiz.svelte
+++ src/lib/components/Quiz.svelte
@@ -13,10 +13,9 @@
 	export let description;
 </script>
 
-<div class="card w-96 bg-base-100 shadow-xl image-full">
-	<figure>
-		<img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
-	</figure>
+<div
+	class="card w-96 bg-base-300 shadow-xl image-full hover:scale-105 transition-all duration-300 cursor-pointer"
+>
 	<div class="card-body">
 		{#if completed}
 			<svg
@@ -26,15 +25,14 @@
 				width="24"
 				height="24"
 				viewBox="0 0 24 24"
-				><path
-					d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"
-				/></svg
 			>
+				<path
+					d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"
+				/>
+			</svg>
 		{/if}
+
 		<h2 class="card-title">{title}</h2>
 		<p>{description}</p>
-		<div class="card-actions justify-end">
-			<button class="btn btn-primary">Buy Now</button>
-		</div>
 	</div>
 </div>
